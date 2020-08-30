b011172ab38864c6230b22db3b90f09bb66ae17d
Maksym Teplyy <maksym.teplyy@atlas-iac.com>
2020-08-30 23:45:52 +0300

    Add github api

---
+++ src/api/domain/github/create_repo_test.go
@@ -0,0 +1,30 @@
+package github
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// TestCreateRepoRequestAsJSON tests converting CreateRepoRequest to and from json properly
+func TestCreateRepoRequestAsJSON(t *testing.T) {
+	request := CreateRepoRequestData{
+		Name:        	"golang introduction",
+		Description: 	"a golang introduction repository",
+		Homepage:    	"https://github.com",
+		Private:     	true,
+		HasIssues:   	false,
+		HasProjects: 	true,
+		HasWiki:     	false,
+	}
+
+	bytes, err := json.Marshal(request)
+	assert.Nil(t, err)
+	assert.NotNil(t, bytes)
+
+	var target CreateRepoRequestData
+	err = json.Unmarshal(bytes, &target)
+	assert.Nil(t, err)
+	assert.EqualValues(t, target.Name, request.Name)
+	assert.EqualValues(t, target.HasIssues, request.HasIssues)
+}
